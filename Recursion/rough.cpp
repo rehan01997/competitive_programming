45f7ff02f1d157d2b178dc7ee9f2223cc92b67f3
rehan01997 <34905911+rehan01997@users.noreply.github.com>
2020-02-28 23:47:22 +0530

    Recursion

--- Recursion/rough.cpp
+++ Recursion/rough.cpp
@@ -1,31 +1,87 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void rabbit(int x,int y,int a,int b){
+void prob4(long long int n,long long int p)
+{
+    
 
-    int count=0;
-    //int ans=0;
-    for(int i=x,j=y;i<=y && j>=x;i+a,j-b){
-        cout<<i;
-        // if(i==j){
-        //     ans=count;
-        // }
-        count++;
-   
-        
+    vector<long long int> vect(n);
+
+    for(int i = 0;i<n;i++){
+        cin>>vect[i];
     }
-    //  if(ans==0){
-    //      return -1;
-    //  } 
-    //  else{
-    //      return ans;
-    //  }  
-    
-    
-    
+
+    vector<int> find;
+
+    for(int i = 0;i<n;i++)
+    {
+        if(vect[i]==1 || vect[i]==p) continue;
+
+        if(vect[i] > p)
+        {
+            cout<<"YES ";
+            for(int j = 0;j<n;j++)
+            {
+                if(j==i) cout<<"1"<<" ";
+                else cout<<"0"<<" ";
+            }
+            return;
+        }
+
+        if(p%vect[i] !=0)
+        {
+            long long int z = p/vect[i];
+
+            cout<<"YES ";
+            for(int j = 0;j<n;j++)
+            {
+                if(j==i) cout<<z+1<<" ";
+                else cout<<"0"<<" ";
+            }
+            return;
+
+        }
+            if(find.size())
+            {
+                for(int k = 0;k<find.size();k++)
+                {
+                    int rem = p-vect[i];
+                    
+                    if(rem%vect[find[k]]!=0)
+                    {
+                        cout<<"YES ";
+                        for(int j = 0;j<n;j++)
+                        {
+                            if(j==find[k])
+                                cout<<rem/vect[find[k]]+1<<" ";
+                            else if(i==j) cout<<1<<" ";
+                            else cout<<"0 ";
+                        }
+                        return ;
+                    }
+                }
+                find.push_back(i);
+
+            }
+
+           if(find.size()==0){
+                find.push_back(i);
+            }
+    }
+
+    cout<<"NO";
 }
+
 int main() {
-    int x,y,a,b;
-    cin>>x>>y>>a>>b;
-    rabbit(x,y,a,b);
+    int t; cin>>t;
+
+    while(t--)
+    {
+        long long int n,p; 
+        cin>>n>>p;
+        prob4(n,p);
+        cout<<endl;
+    }
+
+
 }
