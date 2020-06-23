2dfa92e1e93edefb02745f009ddbf25e523eccd6
rehan01997 <34905911+rehan01997@users.noreply.github.com>
2020-06-23 17:40:26 +0530

    Binary Tree

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -68,6 +68,202 @@ void display(Node *& root)
     display(root->right);
     return;
 }
+int sizeBinaryTree(Node *& node)
+{
+    if(node == NULL) return 0;
+    int c1 = 0 , c2 = 0;
+    if( node->left != NULL)
+    {
+        c1 = sizeBinaryTree(node->left);
+    }
+    if(node->right != NULL)
+    {
+        c2 = sizeBinaryTree(node->right);
+    }
+    return c1 + c2 + 1;
+}
+
+int sumBinaryTre(Node *& node)
+{
+    if( node == NULL) return 0;
+    int s1 = 0;
+    int s2 = 0;
+    if( node->left != NULL)
+    {
+        s1 = sumBinaryTre(node->left);
+    }
+    if(node->right != NULL)
+    {
+        s2 = sumBinaryTre(node->right);
+    }
+    return s1 + s2 + node->data;
+}
+int maxBinaryTree(Node *& node)
+{
+    if(node == NULL) return INT_MIN;
+      
+    int leftMax = maxBinaryTree(node->left);
+    int rightMax = maxBinaryTree(node->right);
+      
+    return max(max(leftMax,rightMax) , node-> data);
+}
+
+int heightBinaryTree(Node *& node) {
+    if(node == NULL) return -1;
+    int leftHeight = heightBinaryTree(node->left);
+    int rightHeight = heightBinaryTree(node->right);
+      
+    return max(leftHeight , rightHeight) + 1;
+}
+bool find(Node *node, int data)
+{
+    if(node == NULL) return false;
+    if(node->data == data) return true;
+    
+    if(node->left != NULL)
+    {
+        bool recAns = find(node->left , data);
+        if(recAns == true) return true;
+    }
+    if(node->right != NULL)
+    {
+        bool recAns = find(node->right , data);
+        if(recAns == true) return true;
+    }
+    return false;
+}
+
+vector<int>nodeToRootPath(Node * node, int data)
+{
+    if( node == NULL)
+    {
+        vector<int>baseAns;
+        return baseAns;
+    }
+    if(node->data == data)
+    {
+        vector<int>base;
+        base.push_back(node->data);
+        return base;
+    }
+    vector<int>ans;
+    if(node->left != NULL)
+    {
+        vector<int>recAns = nodeToRootPath(node->left , data);
+        for(int n : recAns)
+        {
+            ans.push_back(n);
+        }
+    }
+    if(node->right != NULL)
+    {
+        vector<int>recAns = nodeToRootPath(node->right , data);
+        for(int n : recAns)
+        {
+            ans.push_back(n);
+        }
+    }
+    if( ans.size() > 0)
+    {
+        ans.push_back(node->data);
+    }
+    return ans;
+}
+
+void PrePostAndInOrder(Node *&root)
+{
+	stack < pair< Node* , int >> st;
+	st.push( { root , 0 });
+
+	string preOrder = "";
+	string postOrder = "";
+	string inOrder = "";
+	while(!st.empty())
+	{
+		pair<Node* , int > curr= st.top();
+		
+		if(curr.second == 0)
+		{	
+			st.pop();
+			st.push({ curr.first , curr.second + 1});
+			preOrder += curr.first->data + " ";
+		}	
+		else if(curr.second == 1)
+		{	//left
+			st.pop();
+			st.push({ curr.first , curr.second + 1});
+			if( curr.first->left != NULL)
+			{
+				st.push({ curr.first->left , 0 });
+			}
+		}
+		else if( curr.second == 2)
+		{
+			st.pop();
+			st.push({ curr.first , curr.second + 1});
+			inOrder += curr.first->data + " ";
+		}
+		else if(curr.second == 3)
+		{	
+			//right
+			st.pop();
+			st.push({ curr.first , curr.second + 1});
+			if( curr.first->right != NULL)
+			{
+				st.push({ curr.first->right , 0 });
+			}
+		}
+		else
+		{			
+			//st.push({ curr.first , curr.second + 1 });
+			// st.pop();
+			// st.push({ curr.first , curr.second + 1});
+			postOrder += curr.first->data + " ";
+			st.pop();
+		}
+	}
+
+	cout << preOrder << endl;
+	cout << inOrder << endl;
+	cout << postOrder <<endl;
+	return;
+}
+void printKlevelsDown(Node*& root , int k)
+{
+	if( root == NULL) return;
+	else if( k == 0)
+	{
+		cout << root->data << endl;
+		return;
+	}
+	printKlevelsDown(root->left , k - 1 );
+	printKlevelsDown(root -> right , k - 1);
+}
+void levelOrderBinaryTree(Node *& root)
+{
+	queue<Node*>q;
+	q.push(root);
+
+	while( !q.empty())
+	{
+		int size = q.size();
+		for(int i = 0 ; i < size ; i++)
+		{
+			Node* cn = q.front() ; q.pop();
+			cout << cn->data << " " ;
+
+			if( cn->left != NULL) 
+			{
+				q.push(cn->left);
+			}
+			if( cn->right != NULL)
+			{
+				q.push(cn->right);
+			}
+		}
+		cout<<endl;
+	}
+}
 int main()
 {
     int n;
@@ -94,7 +290,32 @@ int main()
     //     cout<<arr[i] << " ";
     // }
     Node * root = construct(arr);
-    display(root);
+    //display(root);
+
+    cout << "size : " << sizeBinaryTree(root) << endl;
+    cout << "sum : " << sumBinaryTre(root) << endl;
+    cout << "max : " << maxBinaryTree(root) << endl;
+    cout << "height : " << heightBinaryTree(root) << endl; 
+
+    // int val; cin >> val;
+    // bool ans = find(root , val);
+    // if( ans == 0) cout << "false";
+    // else cout << "true"; cout<<endl;
+
+    
+    // vector<int> anss = nodeToRootPath( root , val);
+    // for(auto an : anss)
+    // {
+    // 	cout << an << " ";
+    // }
+
+   // PrePostAndInOrder(root);
+
+    // int k ; cin >> k; 
+    // printKlevelsDown(root , k);
+    
+    //levelOrderBinaryTree(root);
+
     return 0 ;
 }
 //19
