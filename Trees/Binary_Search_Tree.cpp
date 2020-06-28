8dd79b919048127de161dad90426dc0f425b94f6
rehan01997 <34905911+rehan01997@users.noreply.github.com>
2020-06-28 21:01:29 +0530

    Binary Tree

--- Trees/Binary_Search_Tree.cpp
+++ Trees/Binary_Search_Tree.cpp
@@ -102,8 +102,126 @@ bool isBST( Node *& root)
 }
 //************************************
 
+int size_bst(Node *& root)
+{
+    if( root == NULL) return 0;
+
+    int l_size = size_bst( root->left );
+    int r_size = size_bst( root -> right );
+
+    return l_size + r_size + 1;    
+}
+int sum_bst( Node *& root)
+{
+    if( root == NULL) return 0;
+    
+    int l_Sum = sum_bst( root -> left);
+    int r_sum = sum_bst( root -> right);
+
+    return l_Sum + r_sum + root -> data;
+}
+// int maxx = INT_MIN;
+int max_bst( Node *& root)
+{   
+    Node * temp = root;
+    if( temp == NULL) return INT_MIN;
+    while( temp -> right != NULL)
+    {
+        temp = temp -> right;
+    }
+    return temp -> data;
+}
+int min_bst( Node *& root)
+{
+    Node * temp = root;
+    if( temp == NULL) return INT_MAX;
+    while( temp -> left != NULL)
+    {
+        temp = temp -> left;
+    }
+    return temp -> data;
+}
+bool find_bst(Node *& root , int data)
+{
+    Node * temp = root;
+    while( temp != NULL)
+    {
+        if( temp -> data == data) return true;
+        else if( data > temp -> data) temp = temp ->right;
+        else temp = temp -> left;
+    }
+    return false;  
+}
+Node * add_bst( Node *& temp , int data)
+{
+    if( temp == NULL )
+    {
+        Node * x = new Node(data);
+        return x;
+    }
+    if( temp -> data == data) return temp;
+
+    if( data > temp -> data ) 
+    {
+        temp -> right = add_bst( temp -> right , data);
+    }
+    else
+    {
+        temp -> left = add_bst( temp -> left , data);
+    }
+    return temp;  
+}
+//******replce with node with the sum of largest nodes**
+int sum = 0 ;
+void rwsol(Node *& temp)
+{
+    if( temp == NULL) return;
+    
+    rwsol( temp -> right);
+    int oval = temp ->data;
+    temp -> data = sum;
+    sum += oval;
+    rwsol( temp ->left);
+    return;
+}
+Node * removeBst( Node *& root , int data)
+{
+    if( root == NULL) return NULL;
+
+    if( root -> data == data)
+    {
+        if( root -> left == NULL && root -> right == NULL) return NULL;
+        else if( root -> left == NULL)
+        {
+            Node * rc = root -> right;
+            root -> right = NULL;
+            return rc ;
+        }
+        else if( root -> right == NULL)
+        {
+            Node * lc = root -> left;
+            root -> left = NULL;
+            return lc;
+        }
+        else
+        {
+            int l_max = max_bst( root -> left);
+            root -> data = l_max;
+            root -> left = removeBst(root->left , l_max);
+        }        
+    }
+    else if( data > root ->data)
+    {
+        root -> right = removeBst( root -> right , data);
+    }
+    else
+    {
+        root -> left = removeBst( root -> left ,data);
+    }
+    return root;    
+}
 int main()
-{    int n;
+{   int n;
     cin >> n;
     vector<string> tree(n);
     for(int i = 0 ; i < n ; i++)
@@ -128,8 +246,30 @@ int main()
     // }
     Node * root = construct(arr);
     //display(root);
-    bool ans = isBST(root);
-    if( ans) cout << "true";
-    else cout << "false";
+    // bool ans = isBST(root);
+    // if( ans) cout << "true";
+    // else cout << "false";
+
+
+    // int ele ; cin >> ele;
+    // cout << size_bst( root ) << endl;
+    // cout << sum_bst( root ) << endl;
+    // cout << max_bst( root ) << endl;
+    // cout << min_bst( root ) << endl;
+    // bool ans = find_bst( root , ele);
+    // if( ans ) cout <<"true";
+    // else cout << "false";
+
+
+    // int ele ; cin >> ele;
+    // root = add_bst( root , ele );
+    // display( root );
+    
+    // rwsol( root );
+    // display( root );
+    
+    int ele ; cin >> ele;
+    removeBst( root , ele);
+    display(root);
     return 0;
 }
